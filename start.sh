4f411b0c792f6875f596472c066c9583d879f15b
Salman Alkhalifah <138271238+SalmanSaleh1@users.noreply.github.com>
2024-09-10 07:04:22 +0300

    make start.sh delete run and image and rebuild and rerun

--- start.sh
+++ start.sh
@@ -1,2 +1,17 @@
-docker build -t ree .
-docker run -p 5000:5000 ree
+#!/bin/bash
+
+# Set the image name
+IMAGE_NAME="ree"
+CONTAINER_NAME="ree_container"
+
+# Delete the existing Docker container if it exists
+docker rm -f $CONTAINER_NAME
+
+# Delete the existing Docker image
+docker rmi -f $IMAGE_NAME
+
+# Build the new Docker image
+docker build -t $IMAGE_NAME .
+
+# Run the Docker container
+docker run -d --name $CONTAINER_NAME -p 5000:5000 $IMAGE_NAME
